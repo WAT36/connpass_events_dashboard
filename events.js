566818e7f11286dd3b6f6154c068556cc6bc6e5d
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-01-22 15:29:06 +0900

    仮でとりあえずデータとってくる関数追加

---
+++ events.js
@@ -0,0 +1,23 @@
+
+// 取得し表示するデータ
+let data = []
+
+// データをAPI経由で取得する関数
+function get_data(){
+
+    // TODO APIで取得する処理
+
+    // 後で直す
+    data = [
+        {
+            "title": "タイトルA"
+        },
+        {
+            "title": "タイトルB"
+        },
+        {
+            "title": "タイトルC"
+        }
+    ]
+    
+}
